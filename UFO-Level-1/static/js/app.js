337bc114e04cfe41bad41e2543a75792824386f8
Ryan Curt <rclassit@ncsu.edu>
2021-03-10 20:19:57 -0500

    added data

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -11,3 +11,14 @@ var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
 var columns = ["datetime","city","state","country","shape","durationMinutes","comments"]
+
+//input data for HTML 
+
+var Data = (datainput) => {
+    datainput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column]))
+    });
+}
+
+Data(tableData);
