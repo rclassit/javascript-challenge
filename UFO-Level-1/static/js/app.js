afae22a56367c5099d25155ce2f728ca2e89a51a
Ryan Curt <rclassit@ncsu.edu>
2021-03-10 20:14:27 -0500

    set up references

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// View data from data.js 
+console.log(tableData);
+
+//references
+
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime","city","state","country","shape","durationMinutes","comments"]
