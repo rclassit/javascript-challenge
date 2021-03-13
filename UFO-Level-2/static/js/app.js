185dd5027ddc25e62b827516b272412cb22e204a
Ryan Curt <rclassit@ncsu.edu>
2021-03-13 11:50:46 -0500

    button build out

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -23,57 +23,13 @@ tableData.forEach(function(ufoSighting) {
     });
   });
 
-//Event listener 
-button.on("click", () => {
-    d3.event.preventDefault();
+//Button build out 
+var button = d3.select("#filter-btn");
+button.on("click", function() {
 
-    var inputDate = inputFieldDate.property("value").trim();
-    // console.log(inputDate)
+    tbody.html("");
 
-    var inputCity = inputFieldCity.property("value").toLowerCase().trim();
-    // console.log(inputCity)
-    // Other types of inputs
-    // var inputState = inputFieldState.property("value").toLowerCase().trim();
-    // var inputCountry = inputFieldCountry.property("value").toLowerCase().trim();
-    // var InputShape = inputFieldShape.property("value").toLowerCase().trim();
-
-    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-    // console.log(filterDate)
-    var filterCity = tableData.filter(tableData => tableData.city === inputCity);
-    // console.log(filterCity)
-    // other filter inputs 
-    // filterState = tableData.filter(tableData => tableData.state === inputState);
-    // filterCountry = tableData.filter(tableData => tableData.country === inputCountry;
-    // filterShape = tableData.filter(tableData => tableData.state === inputShape);
-
-    var filterCombined = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity);
-    // var filterCombinedData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity && tableData.state === inputState && tableData.country === inputCountry && tableData.shape === inputShape);
-    // var filterCombinedDateState = tableData.filter(tableData => tableData.datetime === inputDate && tableData.state === inputState);
-
-    $tbody.html("");
-
-    let response = {
-        filterDate, filterCity, filterCombined
-    }
-
-     // let response = {
-    //     filterDate, filterCity, filterCombinedData, filterState, filterCountry, filterShape
-    // }
-
-    // if(response.filterDate.length !== 0) {
-    //     Data(filterDate);
-    // }
-
-
-    if(response.filterCombined.length !==0 ) {
-        Data(filterCombined);
-    }
-
-        else if(response.filterCombined.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
-            Data(filterDate) || Data(filterCity);
-        }
-
-        else {
-            $tbody.append("tr").append("td").text("No UFO Sightings");
-        }
+    //Select inputs for all columns 
+    var Inputs = d3.select("#input");
+    
 })
